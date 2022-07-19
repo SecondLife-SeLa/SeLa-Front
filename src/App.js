185234e5b0ba28b0a52e13de9cd2d9581dfca932
imjmjmim <imjmjmim0531@gmail.com>
2022-07-19 20:38:47 +0900

    에러

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const PokedexHeader = ({name}) => {
+  return (
+    <h1>This is a Header  for {name} Pokedex!</h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          b2 jimin!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PokedexHeader name={"Jimin"}/>
     </div>
   );
 }
