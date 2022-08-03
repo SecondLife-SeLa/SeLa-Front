bf7ca042c9dffa26929e86481545e7f28708df63
eungbin <50731733+eungbin@users.noreply.github.com>
2022-08-04 00:49:28 +0900

    0804

--- src/App.js
+++ src/App.js
@@ -4,19 +4,7 @@ import {} from "semantic-ui-react";
 import MainPage from "./components/mainPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Job from "./components/job";
-import Management from "./components/JobPage/management";
-import Finance from "./components/JobPage/finance";
-import Coding from "./components/JobPage/coding";
-import NoMatterduty from "./components/JobPage/noMatterDuty";
-import NoMatterRegion from "./components/JobPage/noMatterRegion";
-import Seoul from "./components/JobPage/seoul";
-import Incheon from "./components/JobPage/incheon";
-import Busan from "./components/JobPage/busan";
-import NoMatterEdu from "./components/JobPage/noMatterEdu";
-import Highschool from "./components/JobPage/highschool";
-import Bachelor from "./components/JobPage/bachelor";
-import Doctorate from "./components/JobPage/doctorate";
-import OOcompany from "./components/CompanyInfo/OOcompany";
+import Management from "./components/management";
 
 function App() {
   return (
@@ -27,18 +15,6 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/job" element={<Job />} />
           <Route path="/management" element={<Management />} />
-          <Route path="/finance" element={<Finance />} />
-          <Route path="/coding" element={<Coding />} />
-          <Route path="/seoul" element={<Seoul />} />
-          <Route path="/incheon" element={<Incheon />} />
-          <Route path="/busan" element={<Busan />} />
-          <Route path="/highschool" element={<Highschool />} />
-          <Route path="/bachelor" element={<Bachelor />} />
-          <Route path="/doctorate" element={<Doctorate />} />
-          <Route path="/noMatterDuty" element={<NoMatterduty />} />
-          <Route path="/noMatterRegion" element={<NoMatterRegion />} />
-          <Route path="/noMatterEdu" element={<NoMatterEdu />} />
-          <Route path="/OOcompany" element={<OOcompany />} />
         </Routes>
       </div>
     </BrowserRouter>
