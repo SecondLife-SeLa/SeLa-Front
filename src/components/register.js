b100c6ce53d57f237694fe58f6dcee02ec80394c
lokloc99 <juhan.kwon@outlook.kr>
2022-08-06 15:14:10 +0900

    회원가입

--- src/components/register.js
+++ src/components/register.js
@@ -1,13 +1,13 @@
-import { useRef, useState, useEffect
-} from "react";
-//npm i @fortawesome/react-fontawesome @fortawesome/free-solid-svg-icons @fortawesome/fontawesome-svg-core//
-import {faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons"
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from './api/axios';
 import './register.css'
 
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/register';
 
-const USER_REGEX = /^[a-zA-Z][a-zA-z0-9-_]{3,23}$/; 
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%]).{8,24}$/;
 
 const Register = () => {
     const userRef = useRef();
@@ -22,8 +22,8 @@ const Register = () => {
     const [pwdFocus, setPwdFocus] = useState(false);
 
     const [matchPwd, setMatchPwd] = useState('');
-    const[validMatch, setValidMatch] = useState(false);
-    const[matchFocus, setMatchFocus] = useState(false);
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
 
     const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
@@ -33,69 +33,153 @@ const Register = () => {
     }, [])
 
     useEffect(() => {
-        const result = USER_REGEX.test(user);
-        console.log(result);
-        console.log(user);
-        setValidName(result);
+        setValidName(USER_REGEX.test(user));
     }, [user])
 
-    
     useEffect(() => {
-        const result = PWD_REGEX.test(pwd);
-        console.log(result);
-        console.log(pwd);
-        setValidPwd(result);
-        const match = pwd === matchPwd;
-        setValidMatch(match);
-    }, [user, pwd, matchPwd])
+        setValidPwd(PWD_REGEX.test(pwd));
+        setValidMatch(pwd === matchPwd);
+    }, [pwd, matchPwd])
 
     useEffect(() => {
         setErrMsg('');
     }, [user, pwd, matchPwd])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // if button enabled with JS hack
+        const v1 = USER_REGEX.test(user);
+        const v2 = PWD_REGEX.test(pwd);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
+        }
+        try {
+            const response = await axios.post(REGISTER_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(response?.data);
+            console.log(response?.accessToken);
+            console.log(JSON.stringify(response))
+            setSuccess(true);
+            //clear state and controlled inputs
+            //need value attrib on inputs for this
+            setUser('');
+            setPwd('');
+            setMatchPwd('');
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 409) {
+                setErrMsg('Username Taken');
+            } else {
+                setErrMsg('Registration Failed')
+            }
+            errRef.current.focus();
+        }
+    }
+
     return (
-        <section>
-            <p ref={errRef} className={errMsg ? "errmsg" :
-            "offscreen"} aria-live="assertive">{errMsg}</p>
-            <h1>회원가입</h1>
-            <form>
-                <label htmlFor="username">
-                    Username:
-                    <span className={validName ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon ={faCheck} />
-                    </span>
-                    <span className={validName || !user ? "hide" :
-                    "invalid"}>
-                        <FontAwesomeIcon icon ={faTimes} />
-                    </span>
-                </label>
-                <input
-                    type="text"
-                    id="username"
-                    ref={userRef}
-                    autoComlete="off"
-                    onChange={(e) => setUser(e.target.value)}
-                    required
-                    aria-invalid={validName ? "false" : "true"}
-                    aria-describedby="uidnote"
-                    onFocus={() => setUserFocus(true)}
-                    onBlur={() => setUserFocus(false)}
-                />
-                <p id="uidnote" className={userFocus && user &&
-                !validName ? "instructions": "offscreen"}>
-                    <FontAwesomeIcon icon = {faInfoCircle} />
-                    8~24의 길이로. <br />
-                    영문자로 시작되어야 합니다. <br />
-                    영문자, 숫자, 특수문자와 조합해야 합니다. 
-                </p>
-                      
+        <>
+            {success ? (
+                <section>
+                    <h1>회원가입 성공!</h1>
+                    <p>
+                        <a href="./login">로그인</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <h1>회원가입</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">
+                            아이디:
+                            <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
+                            required
+                            aria-invalid={validName ? "false" : "true"}
+                            aria-describedby="uidnote"
+                            onFocus={() => setUserFocus(true)}
+                            onBlur={() => setUserFocus(false)}
+                        />
+                        <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            4 ~ 24자의 영문, 숫자, 허용된 특수기호로 이루어져야 됩니다.<br />
+                            영문으로 시작해야 됩니다.<br />
+                            문자, 숫자, "_", "-" 사용가능.
+                        </p>
 
-                
-                
-                
-            </form>
 
-        </section>
+                        <label htmlFor="password">
+                            비밀번호:
+                            <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}
+                            required
+                            aria-invalid={validPwd ? "false" : "true"}
+                            aria-describedby="pwdnote"
+                            onFocus={() => setPwdFocus(true)}
+                            onBlur={() => setPwdFocus(false)}
+                        />
+                        <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            8 ~ 24자의 영문, 숫자, 허용된 특수기호로 이루어져야 됩니다.<br />
+                            대문자와 소문자, 숫자 및 특수 문자를 포함되어야 합니다.<br />
+                            사용 가능한 특수기호: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                        </p>
+
+
+                        <label htmlFor="confirm_pwd">
+                            비밀번호 확인:
+                            <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
+                        </label>
+                        <input
+                            type="password"
+                            id="confirm_pwd"
+                            onChange={(e) => setMatchPwd(e.target.value)}
+                            value={matchPwd}
+                            required
+                            aria-invalid={validMatch ? "false" : "true"}
+                            aria-describedby="confirmnote"
+                            onFocus={() => setMatchFocus(true)}
+                            onBlur={() => setMatchFocus(false)}
+                        />
+                        <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            비밀번호와 일치해야 합니다.
+                        </p>
+
+                        <button disabled={!validName || !validPwd || !validMatch ? true : false}>가입</button>
+                    </form>
+                    <p>
+                        이미 가입하셨나요?<br />
+                        <span className="line">
+                            {/*put router link here*/}
+                            <a href="#">가입하기</a>
+                        </span>
+                    </p>
+                </section>
+            )}
+        </>
     )
 }
 
